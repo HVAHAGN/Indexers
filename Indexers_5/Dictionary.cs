bd72ac593a53acb5025d900d13db6c1d2f4d52db
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-03-30 19:37:14 +0400

    Different Indexers in the same Class

---
+++ Indexers_5/Dictionary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers_5
+{
+    class Dictionary
+    {
+        private string[] key = new string[5];
+        private string[] value = new string[6];
+        public Dictionary()
+        {
+            key[0] = "girq"; value[0] = "book";
+            key[1] = "grich"; value[1] = "pencil";
+            key[2] = "arev"; value[2] = "sun";
+            key[3] = "sexan"; value[3] = "table";
+
+
+        }
+        public string this[string index]
+        {
+            get
+            {
+                for (int i = 0; i < key.Length; i++)
+                {
+                    if (key[i] == index)
+                    {
+                        return key[i] + " " + value[i];
+                    }
+
+                }
+                return string.Format("{0}-not found!", index);
+            }
+        }
+
+
+             public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < key.Length)
+                    return key[index] + " - " + value[index];
+                else
+                    return "out of array length! ";
+            }
+        }
+    }
+
+    }
+
---
+++ Indexers_5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary dictionary = new Dictionary();
+            Console.WriteLine(dictionary["girq"]);
+            Console.WriteLine(dictionary["grich"]);
+            Console.WriteLine(dictionary["tun"]);
+            Console.WriteLine(dictionary["arev"]);
+
+
+            Console.WriteLine(new string('*',10));
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(dictionary[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
