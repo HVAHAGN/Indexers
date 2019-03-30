63cf179aea93aeb6ee8b29c1b6e0f5183603f7f8
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-03-30 18:23:01 +0400

    Add Indexers with TwoDimensional

---
+++ Indexers_4/MyClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers_4
+{
+    class MyClass
+    {
+        private int[,] arr = new int[3, 3];
+        
+        //Indexers
+        public int this[int index1, int index2]
+        {
+            get //Accessor
+            {
+                return arr[index1, index2];
+            }
+            set //Mutator
+            {
+                arr[index1, index2] = value;
+
+            }
+        }
+ 
+    }
+}
