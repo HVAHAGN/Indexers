96f18570aefc8e0bf74e146dcb14b8c806c2263b
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-03-30 18:12:15 +0400

    Add MyClass Idexers with for cycle

---
+++ Indexers_3/MyClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers_3
+{
+    class MyClass
+    {
+        private string[] arr = new string[5];
+        //Indexers
+        public string this[int index]
+        {
+            get //Accessor
+            {
+                if (index >= 0 && index < arr.Length)
+                    return arr[index];
+                else
+                    return "This index out of your array range";
+            }
+            set //Mutator
+            {
+                if (index >= 0 && index < arr.Length)
+                    arr[index] = value;
+                else
+                    Console.WriteLine("This index out of your array range");
+
+            }
+        }
+ 
+    }
+}
---
+++ Indexers_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyClass my= new MyClass();
+            for (int i = 0; i < 6; i++)
+            {
+                my[i] = string.Format("string {0}", i);
+
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(my[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
